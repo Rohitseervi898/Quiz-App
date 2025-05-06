7abacdc1f5790a8dfef0f00a29e453b929e256b2
rohit8866seervi <rohitseervi8866@gmail.com>
2025-05-06 15:26:20 +0530

    added score and count to the questions. now 90% of the project in completed.

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,16 +5,16 @@ import QuestionContext from '../context/QuestionContext'
 
 const Questions = () => {
 
-    const {language, difficulty}=useContext(QuestionContext)
+    const {language, difficulty,score,setScore,count,setCount}=useContext(QuestionContext)
 
     let qnumber=Math.floor(Math.random()*100%15+1)
     const questionLanguage=questions.find(q=>q.language===language)
     const questionDifficulty=questionLanguage.difficulty.find(d=>d[difficulty])
     const question=questionDifficulty[difficulty];
-    console.log(questionLanguage)
-    console.log(questionDifficulty)
-    console.log(qnumber)
-    console.log(questionDifficulty[difficulty])
+    // console.log(questionLanguage)
+    // console.log(questionDifficulty)
+    // console.log(qnumber)
+    // console.log(questionDifficulty[difficulty])
 
 
     const navigate=useNavigate()
@@ -22,14 +22,23 @@ const Questions = () => {
     const checkAnswer=(e)=>{
         e.preventDefault()
         const selectedAnswer = e.target.innerText;
-
+        if(selectedAnswer===question[qnumber].answer){
+            setScore(score+1);
+        }
+        setCount(count+1)
+        if(count===5){
+            alert(`Your score is ${score} `)
+            setCount(0);
+            setScore(0);
+            return navigate('/')
+        }
         return navigate('/quiz')
     }
 
     return (
         <>
         <div className='flex flex-col max-w-130 w-102 m-auto justify-center h-screen px-1.5' >
-            <p>1/5</p>
+            <p>{count}/5</p>
             <p className=''>{question[qnumber].question}</p>
             {question[qnumber].options.map((option,index)=>(
                 <button key={index} className='w-full bg-blue-300 border rounded p-1 m-2' onClick={checkAnswer}>{option}</button>
--- src/context/QuestionContextProvider.jsx
+++ src/context/QuestionContextProvider.jsx
@@ -4,9 +4,11 @@ import QuestionContext from "../context/QuestionContext";
 const QuestionContextProvider = ({children})=>{
     const [language,setLanguage]=useState("")
     const [difficulty,setDifficulty] = useState("")
+    const [score,setScore] =useState(0)
+    const [count,setCount]=useState(1)
 
     return(
-        <QuestionContext.Provider value={{language, setLanguage, difficulty, setDifficulty}}>
+        <QuestionContext.Provider value={{language, setLanguage, difficulty, setDifficulty, score,setScore, count, setCount}}>
             {children}
         </QuestionContext.Provider>
     )
